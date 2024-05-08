56f847939a2bd7c3f7df2db57ecd4ac7552da1e5
Motunrayo <ajirotutu4real@gmail.com>
2024-05-08 15:56:11 +0100

    Modified

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -14,3 +14,23 @@ print(validUTF8(data))
 data = [229, 65, 127, 256]
 print(validUTF8(data))
 
+
+data = [195, 169]
+print(validUTF8(data))
+
+test_cases = [
+    [[467, 133, 108], False],
+    [[240, 188, 128, 167], True],
+    [[235, 140], False],
+    [[345, 467], False],
+    [[250, 145, 145, 145, 145], False],
+    [[0, 0, 0, 0, 0, 0], True],
+    [[], True]
+]
+
+# Run test cases
+for data, expected_result in test_cases:
+    result = validUTF8(data)
+    assert result == expected_result, f"Input: {data}, Expected: {expected_result}, Got: {result}"
+
+print("All test cases passed successfully!")
--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,26 +5,26 @@
 def validUTF8(data):
     """Validate if data is a valid UTF-8 char sequence"""
     num_bytes = 0
-    
+
     for byte in data:
-      if num_bytes == 0: #start of sequence or single character
-        if byte >> 7 == 0b0:
-          num_bytes = 0
-        elif byte >> 5 == 0b110:
-          num_bytes = 1
-        elif byte >> 4 == 0b1110:
-          num_bytes = 2
-        elif byte >> 3 == 0b11110:
-          num_bytes = 3
+        # start of sequence or single character
+        if num_bytes == 0:
+            if byte >> 7 == 0b0:
+                num_bytes = 0
+            elif byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            else:
+                return False
         else:
-          return False
-      else:
-        if byte >> 6 != 0b10:
-          return False
-        num_bytes -= 1
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
 
     if num_bytes != 0:
-      return False
-    
+        return False
+
     return True
-    
