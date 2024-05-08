8e044fdcfc75f9767840cd31730b3fcbf659eafc
Motunrayo <ajirotutu4real@gmail.com>
2024-05-08 15:29:30 +0100

    Modifications

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,24 +3,28 @@
 
 
 def validUTF8(data):
-    """Validate UTF-8 encoding"""
-    seq_after = 0
-
+    """Validate if data is a valid UTF-8 char sequence"""
+    num_bytes = 0
+    
     for byte in data:
-        if byte < 128:  # Single-byte character
-            if seq_after > 0:
-                return False
+      if num_bytes == 0: #start of sequence or single character
+        if byte >> 7 == 0b0:
+          num_bytes = 0
+        elif byte >> 5 == 0b110:
+          num_bytes = 1
+        elif byte >> 4 == 0b1110:
+          num_bytes = 2
+        elif byte >> 3 == 0b11110:
+          num_bytes = 3
         else:
-            seq = byte // 128 + 1
-            if seq_after == 0:
-                if seq < 2 or seq > 4:
-                    return False
-                seq_after = seq - 1
-            else:
-                if seq != 1:
-                    return False
+          return False
+      else:
+        if byte >> 6 != 0b10:
+          return False
+        num_bytes -= 1
 
-            seq_after -= 1
-            if seq_after < 0:
-                return False
-    return seq_after == 0
+    if num_bytes != 0:
+      return False
+    
+    return True
+    
